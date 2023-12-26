be7cc4b48ce1538195b04bb456e981ffa54601cd
unknown <omarashvili.giorgi07@gmail.com>
2023-12-27 00:57:00 +0400

    idk what i did

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <span className={styles.mySpan}>:D</span>
+    </main>
+  )
+}
